afaba3b0f22c9b23c1c9a4c6322bc76862ec841e
woolen <349526042@qq.com>
2014-01-08 02:55:34 -0500

    init guard spec

---
+++ spec/test_data/foo_test/foo.c
@@ -0,0 +1,27 @@
+/**
+# Author: Woolen.Wang
+# Email: just_woolen＠qq.com
+# File Name: foo.c
+=========================================================================
+Description: 
+这个是测试注释生成单元测试的
+Edit History: 
+2014-01-07    File created.
+=========================================================================
+**/
+#include "foo.h"
+/**
+* 这个函数就是测测两个谁大
+* param a 数1
+* param b 数2
+*/
+/**
+*unit_test
+*input 1,2 should return 2
+*input 1,3 should return 3
+*input 2,4 should return 4
+*/
+int max(int a, int b)
+{
+    return a>b?a:b;
+}
---
+++ spec/test_data/foo_test/foo.h
@@ -0,0 +1,16 @@
+/**
+# Author: Woolen.Wang
+# Email: just_woolen＠qq.com
+# File Name: foo.h
+=========================================================================
+ Description: 
+   测一下gtest
+ Edit History: 
+   2014-01-07    File created.
+=========================================================================
+**/
+#ifndef __FOO_H__
+#define __FOO_H__
+int max(int a, int b);
+
+#endif
